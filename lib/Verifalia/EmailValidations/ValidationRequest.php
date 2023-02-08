653434cd9fc1410be4c26d1a79a72f190098b119
socialpresencenetwork <115162924+socialpresencenetwork@users.noreply.github.com>
2023-02-07 22:40:16 -0500

    Add options

--- lib/Verifalia/EmailValidations/ValidationRequest.php
+++ lib/Verifalia/EmailValidations/ValidationRequest.php
@@ -10,7 +10,7 @@ namespace Verifalia\EmailValidations {
 		public $priority = null;
 		public $retention = null;
 
-		public function __construct($entries)
+		public function __construct($entries, $options = [])
 		{
 			$this->entries = array();
 
@@ -21,6 +21,10 @@ namespace Verifalia\EmailValidations {
 			} else {
 				$this->addEntry($entries);
 			}
+
+            if (!empty($options))
+                $this->setOptions($options);
+
 		}
 
 		private function addEntry($entry)
@@ -33,7 +37,64 @@ namespace Verifalia\EmailValidations {
 				throw new \InvalidArgumentException('Invalid input entries, please review the data you are about to submit to Verifalia.');
 			}
 		}
-	}
+
+        private function setOptions(array $options) : void
+        {
+            if (isset($options['quality']))
+                $this->setQuality($options['quality']);
+
+            if (isset($options['deduplication']))
+                $this->setDeduplication($options['deduplication']);
+
+            if (isset($options['priority']))
+                $this->setPriority($options['priority']);
+
+            if (isset($options['retention']))
+                $this->setRetention($options['retention']);
+        }
+
+
+        /**
+         * @param 'Standard'|'High'|'Extreme' $quality
+         */
+        public function setQuality(string $quality) : void
+        {
+            if (!in_array($quality, ['Standard', 'High', 'Extreme']))
+                return;
+            $this->quality = $quality;
+        }
+
+        /**
+         * @param 'Off'|'Safe'|'Relaxed' $dedupe
+         */
+        public function setDeduplication(string $dedupe) : void
+        {
+            if (!in_array($quality, ['Off', 'Safe', 'Relaxed']))
+                return;
+            $this->deduplication = $dedupe;
+        }
+
+        /**
+         * @param $priority
+         */
+        public function setPriority(int $priority) : void
+        {
+            if ($priority < 0 || $priority > 255)
+                return;
+            $this->priority = $priority;
+        }
+
+        public function setRetention(string $retention) : void
+        {
+            if (empty($retention))
+                return;
+
+            $this->retention = $retention;
+
+        }
+
+
+    }
 }
 
 ?>
